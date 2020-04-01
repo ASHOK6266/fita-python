386f00602bb6b54fd57cde4a9efe649acf2d7d54
ashokba6266 <ashokba6266@gmail.com>
2020-04-01 16:22:57 +0200

    learnt the type conversion

--- python/data-types.py
+++ python/data-types.py
@@ -11,7 +11,7 @@ print(sampleString.replace(" ","-"))
 The below is to check for palindrome
 '''
 
-string = "level"
+string = 'radar'
 var1 = string[:]
 var2 = string[::-1]
 #print(var2)
@@ -19,3 +19,23 @@ if(var1 == var2):
     print("The given variable is palindrome")
 else:
     print("The variable is not a palindrome")
+
+'''
+Type conversion
+'''
+#Integer to float
+
+num1 = 24
+print(float(num1))
+
+#Integer to string conversion
+num2 = 22
+converted_variable = str(num2)
+print(type(converted_variable))
+
+#Float to integer conversion
+
+float_num = 45.44
+print(int(float_num))
+
+
