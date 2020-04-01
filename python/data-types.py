6e6c0d18ab445b8ce2b757b12db5d51c7460a823
ashokba6266 <ashokba6266@gmail.com>
2020-04-01 15:35:50 +0200

    done with palirome

--- python/data-types.py
+++ python/data-types.py
@@ -6,3 +6,16 @@ Python3
 sampleString = "This is a string a and I am going to replace the white spaces with the hyphen"
 
 print(sampleString.replace(" ","-"))
+
+'''
+The below is to check for palindrome
+'''
+
+string = "level"
+var1 = string[:]
+var2 = string[::-1]
+#print(var2)
+if(var1 == var2):
+    print("The given string is palindrome")
+else:
+    print("The string is not a palindrome")
