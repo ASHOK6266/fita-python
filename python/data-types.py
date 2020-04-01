fd280d1bde73525e44e729914b1257bb2efac340
ashokba6266 <ashokba6266@gmail.com>
2020-04-01 12:37:47 +0200

    Completed the exercise using in-built function

---
+++ python/data-types.py
@@ -0,0 +1,8 @@
+'''
+The below example is for using a replace in-built function
+Python3
+'''
+
+sampleString = "This is a string a and I am going to replace the white spaces with the hyphen"
+
+print(sampleString.replace(" ","-"))
