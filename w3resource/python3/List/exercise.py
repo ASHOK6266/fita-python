f6b60397e529c7ac3ea5c1aeda3eb5452f71d8b0
ashokba6266 <ashokba6266@gmail.com>
2020-04-03 11:44:46 +0200

    worked on list exercise

---
+++ w3resource/python3/List/exercise.py
@@ -0,0 +1,49 @@
+'''
+
+1. Write a Python program to sum all the items in a list.
+
+
+
+list_values = [2,4,5,6]
+
+index = 0
+while index < len(list_values):
+    print(list_values[index])
+    index += 1
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+2. Write a Python program to multiplies all the items in a list. 
+
+
+
+3. Write a Python program to get the largest number from a list.
+
+
+
+lis = [2,7,8,89,78,98,87,34]
+large = max(lis)
+print(large)
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+5. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
+
+
+HOW TO FIND THE LENGTH
+
+sample =  ['abc', 'xyz', 'aba', '1221','1']
+
+for x in sample:
+    if len(x) >= 2 and x[0] == x[-1]:
+        print(x)
+'''
+
+7. Write a Python program to remove duplicates from a list
+
+sampleList = ['1','ak','ak','1','5']
+
+
+
+
+
+
