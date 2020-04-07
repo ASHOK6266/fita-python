827d32370b9deb86dc1de3c4c34f7b65641bf94d
ashokba6266 <ashokba6266@gmail.com>
2020-04-07 12:13:24 +0200

    working on data type

---
+++ w3resource/data-types/exercise.py
@@ -0,0 +1,8 @@
+'''
+14. Write a Python program to find the first appearance of the substring 'not' and 'poor' from a given string, if 'bad' follows the 'poor', 
+replace the whole 'not'...'poor' substring with 'good'. Return the resulting string. Go to the editor
+Sample String : 'The lyrics is not that poor!'
+Expected Result : 'The lyrics is good!'
+
+'''
+
