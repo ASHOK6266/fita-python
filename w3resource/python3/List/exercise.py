17f3a7f6117e66f8f5e6efb5d5168090567aae2f
ashokba6266 <ashokba6266@gmail.com>
2020-04-08 23:39:05 +0200

    status

--- w3resource/python3/List/exercise.py
+++ w3resource/python3/List/exercise.py
@@ -3,13 +3,14 @@
 1. Write a Python program to sum all the items in a list.
 
 
-
 list_values = [2,4,5,6]
 
 index = 0
 while index < len(list_values):
     print(list_values[index])
     index += 1
+
+
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 2. Write a Python program to multiplies all the items in a list. 
@@ -26,23 +27,26 @@ print(large)
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-5. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
+
+5. Write a Python program to count the number of strings where the string length is 2 or more 
+and the first and last character are same from a given list of strings.
 
 
-HOW TO FIND THE LENGTH
+#HOW TO FIND THE LENGTH
 
-sample =  ['abc', 'xyz', 'aba', '1221','1']
+sample =  ['abc', 'xyz', 'aba', '1221','1','121']  # x[0] = a
 
 empty = []
-empty2 =[]
+#empty2 =[]
 
 #empty2.extend()
 
 for x in sample:
     if len(x) >= 2 and x[0] == x[-1]:
         empty.append(x)
-        empty2.extend(list(x)) # The return format will be in the form of list
-print(len(empty))
+        #empty2.extend(list(x)) # The return format will be in the form of list
+print(len(empty)
+
 
 7. Write a Python program to remove duplicates from a list
 
@@ -60,11 +64,13 @@ list1.extend(se)
 list2.append(tup) 
 
 print(list2)
-'''
+
 dict3 = {'as': 33, 'ak': 33, 'an': 33}
 
-'''
+
 for k,v in dict3.items():
     #print(k)
-'''    
+    
 print(dict3.values())
+
+'''
