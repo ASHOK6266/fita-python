f1dafeb51ad50018a2f8a022738a851801bfbbcf
ashokba6266 <ashokba6266@gmail.com>
2020-04-16 23:51:07 +0200

    Tested the start command bot

--- bot/app.py
+++ bot/app.py
@@ -1,29 +1,19 @@
+from telegram.ext import Updater, CommandHandler
 import logging
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                     level=logging.INFO)
 
-logger = logging.getLogger(__name__)
+updater = Updater(token="1138658257:AAGrzbG6EWU60dt4vJ6XAeUH1ppv7r6oA30",use_context=True)
 
-
-def echo(update, context):
-    """Echo the user message."""
-    db = update.message.reply_text(update.message.text)
+dispatcher = updater.dispatcher
 
 
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+start_Handler = CommandHandler('start',start)
+dispatcher.add_handler(start_Handler)
 
-def main():
-    updater = Updater("1138658257:AAGrzbG6EWU60dt4vJ6XAeUH1ppv7r6oA30",use_context=True)
-
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text,echo))
-
-    updater.start_pollling()
-
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+updater.start_polling()
