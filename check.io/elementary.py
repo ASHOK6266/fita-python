df38756525be161c164b743491ff9418004a87a3
ashokba6266 <ashokba6266@gmail.com>
2020-05-04 06:28:16 +0200

    checkio

--- check.io/elementary.py
+++ check.io/elementary.py
@@ -71,3 +71,50 @@ if __name__ == '__main__':
     assert list(split_pairs('a')) == ['a_']
     assert list(split_pairs('')) == []
     print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+'''
+You are given a string and two markers (the initial one and final). You have to find a substring enclosed between these two markers. But there are a few important conditions.
+
+This is a simplified version of the Between Markers mission.
+
+The initial and final markers are always different.
+The initial and final markers are always 1 char size.
+The initial and final markers always exist in a string and go one after another.
+Input: Three arguments. All of them are strings. The second and third arguments are the initial and final markers.
+
+Output: A string.
+
+Example:
+
+between_markers('What is >apple<', '>', '<') == 'apple'
+1
+How it is used: For text parsing.
+
+Precondition: There can't be more than one final and one initial markers.
+
+'''
+
+def between_markers(text: str, begin: str, end: str) -> str:
+    """
+        returns substring between two given markers
+    """
+    # your code here
+    
+    begin_index = text.index(begin)
+    end_index = text.index(end)
+    
+    return text[(begin_index)+1:end_index]
+
+if __name__ == '__main__':
+    print('Example:')
+    print(between_markers('What is >apple<', '>', '<'))
+
+    # These "asserts" are used for self-checking and not for testing
+    assert between_markers('What is >apple<', '>', '<') == "apple"
+    assert between_markers('What is [apple]', '[', ']') == "apple"
+    assert between_markers('What is ><', '>', '<') == ""
+    assert between_markers('>apple<', '>', '<') == "apple"
+    print('Wow, you are doing pretty good. Time to check it!')
+
+
